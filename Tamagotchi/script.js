6f3ae9b4766dcbf35e7184eee78cddfa5cd7ebd8
JacobMowbrayJones <35005363+JacobMowbrayJones@users.noreply.github.com>
2018-03-04 04:46:49 +0800

    Test

--- Tamagotchi/script.js
+++ Tamagotchi/script.js
@@ -1,27 +1,10 @@
-var id = setInterval(frame, 5);
+
+document.getElementById("animateButton").onclick = function() {myMove()};
+
 
 function myMove() {
     console.log("my Move Pressed");
   
-  var elem = document.getElementById("petAnimation");   
-  var pos = 0;
-  var id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++; 
-      elem.style.top = pos + 'px'; 
-      elem.style.left = pos + 'px'; 
-    }
-  }
-  
-}
 
-function frame() {
-    if (/* test for finished */) {
-        clearInterval(id);
-    } else {
-        /* code to change the element style */  
-    }
+  
 }
